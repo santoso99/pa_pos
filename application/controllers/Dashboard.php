8e4614608cd3b3f853a840e823f4e26d2f18853c
Asrul Cahyadi Putra <asrulcahyadiputra99@gmail.com>
2020-12-05 03:51:24 +0700

    Fixed Dashboard

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -5,10 +5,20 @@ defined('BASEPATH') or exit('No direct script access allowed');
 class Dashboard extends CI_Controller
 {
 
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('M_widget', 'model');
+	}
+
+
 	public function index()
 	{
 		$data = [
-			'title'		=> 'Dashboard'
+			'title'				=> 'Dashboard',
+			'sales'				=> $this->model->sales(),
+			'latest_sales'			=> $this->model->latest_sales(),
+			'latest_purchasing'		=> $this->model->latest_purchasing()
 		];
 		$this->load->view('dashboard', $data);
 	}
--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -22,76 +22,21 @@
 					</div>
 				</div>
 				<div class="col-lg-4 col-12">
-					<a href="#" class="box bg-danger bg-hover-danger pull-up">
+					<a href="<?= site_url('transaksi/penjualan') ?>" class="box bg-danger bg-hover-danger pull-up">
 						<div class="box-body">
 							<div class="d-flex align-items-center">
 								<div class="w-80 h-80 l-h-100 rounded-circle b-1 border-white text-center">
 									<span class="text-white icon-Cart2 font-size-40"><span class="path1"></span><span class="path2"></span></span>
 								</div>
 								<div class="ml-10">
-									<h4 class="text-white mb-0">0</h4>
-									<h5 class="text-white-50 mb-0">Total Penjualan</h5>
+									<h4 class="text-white mb-0"><?= nominal1($sales['qty']) ?></h4>
+									<h5 class="text-white-50 mb-0">Produk Terjual</h5>
 								</div>
 							</div>
 						</div>
 					</a>
 				</div>
-				<div class="col-12">
-					<div class="box no-shadow mb-0 bg-transparent">
-						<div class="box-header no-border px-0">
-							<h4 class="box-title">Daftar Brand</h4>
-							<ul class="box-controls pull-right d-md-flex d-none">
-								<li>
-									<button class="btn btn-primary-light px-10">Lihat Semua</button>
-								</li>
-							</ul>
-						</div>
-					</div>
-				</div>
-				<div class="col-lg-3 col-md-6 col-12">
-					<div class="box pull-up">
-						<div class="box-body d-flex align-items-center">
-							<img src="<?= base_url() ?>assets/images/front-end-img/courses/cor-logo-6.png" alt="" class="align-self-end h-80 w-80">
-							<div class="d-flex flex-column flex-grow-1">
-								<h5 class="box-title font-size-16 mb-2">Angular Class</h5>
-								<a href="#">Learn more</a>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="col-lg-3 col-md-6 col-12">
-					<div class="box pull-up">
-						<div class="box-body d-flex align-items-center">
-							<img src="<?= base_url() ?>assets/images/front-end-img/courses/cor-logo-5.png" alt="" class="align-self-end h-80 w-80">
-							<div class="d-flex flex-column flex-grow-1">
-								<h5 class="box-title font-size-16 mb-2">Android Class</h5>
-								<a href="#">Learn more</a>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="col-lg-3 col-md-6 col-12">
-					<div class="box pull-up">
-						<div class="box-body d-flex align-items-center">
-							<img src="<?= base_url() ?>assets/images/front-end-img/courses/cor-logo-4.png" alt="" class="align-self-end h-80 w-80">
-							<div class="d-flex flex-column flex-grow-1">
-								<h5 class="box-title font-size-16 mb-2">Python Class</h5>
-								<a href="#">Learn more</a>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="col-lg-3 col-md-6 col-12">
-					<div class="box pull-up">
-						<div class="box-body d-flex align-items-center">
-							<img src="<?= base_url() ?>assets/images/front-end-img/courses/cor-logo-3.png" alt="" class="align-self-end h-80 w-80">
-							<div class="d-flex flex-column flex-grow-1">
-								<h5 class="box-title font-size-16 mb-2">laravel </h5>
-								<a href="#">Learn more</a>
-							</div>
-						</div>
-					</div>
-				</div>
+
 			</div>
 
 			<div class="row">
@@ -105,168 +50,68 @@
 											<h4 class="box-title">Pembelian Terbaru</h4>
 											<ul class="box-controls pull-right d-md-flex d-none">
 												<li>
-													<button class="btn btn-primary-light px-10">Lihat Semua</button>
+													<a href="<?= site_url('transaksi/pembelian') ?>" class="btn btn-primary-light px-10">Lihat Semua</a>
 												</li>
 											</ul>
 										</div>
 									</div>
-									<div class="box mb-15 pull-up">
-										<div class="box-body">
-											<div class="d-flex align-items-center justify-content-between">
-												<div class="d-flex align-items-center">
-													<div class="mr-15 bg-warning h-50 w-50 l-h-60 rounded text-center">
-														<span class="icon-Book-open font-size-24"><span class="path1"></span><span class="path2"></span></span>
+									<?php foreach ($latest_purchasing as $lp) : ?>
+										<div class="box mb-15 pull-up">
+											<div class="box-body">
+												<div class="d-flex align-items-center justify-content-between">
+													<div class="d-flex align-items-center">
+														<div class="mr-15 bg-warning h-50 w-50 l-h-60 rounded text-center">
+															<span class="icon-Book-open font-size-24"><span class="path1"></span><span class="path2"></span></span>
+														</div>
+														<div class="d-flex flex-column font-weight-500">
+															<a href="<?= site_url('transaksi/pembelian/detail/' . $lp['id_transaksi']) ?>" class="text-dark hover-primary mb-1 font-size-16"><?= $lp['id_transaksi'] ?></a>
+															<span class="text-fade"><?= $lp['nama_vendor'] ?>, <?= mediumdate_indo(date('Y-m-d', strtotime($lp['tanggal']))) ?></span>
+														</div>
 													</div>
-													<div class="d-flex flex-column font-weight-500">
-														<a href="#" class="text-dark hover-primary mb-1 font-size-16">Informatic Course</a>
-														<span class="text-fade">Johen Doe, 19 April</span>
-													</div>
+													<a href="<?= site_url('transaksi/pembelian/detail/' . $lp['id_transaksi']) ?>">
+														<span class="icon-Arrow-right font-size-24"><span class="path1"></span><span class="path2"></span></span>
+													</a>
 												</div>
-												<a href="#">
-													<span class="icon-Arrow-right font-size-24"><span class="path1"></span><span class="path2"></span></span>
-												</a>
 											</div>
 										</div>
-									</div>
-									<div class="box mb-15 pull-up">
-										<div class="box-body">
-											<div class="d-flex align-items-center justify-content-between">
-												<div class="d-flex align-items-center">
-													<div class="mr-15 bg-primary h-50 w-50 l-h-60 rounded text-center">
-														<span class="icon-Mail font-size-24"></span>
-													</div>
-													<div class="d-flex flex-column font-weight-500">
-														<a href="#" class="text-dark hover-primary mb-1 font-size-16">Live Drawing</a>
-														<span class="text-fade">Micak Doe, 12 June</span>
-													</div>
-												</div>
-												<a href="#">
-													<span class="icon-Arrow-right font-size-24"><span class="path1"></span><span class="path2"></span></span>
-												</a>
-											</div>
-										</div>
-									</div>
-									<div class="box mb-15 pull-up">
-										<div class="box-body">
-											<div class="d-flex align-items-center justify-content-between">
-												<div class="d-flex align-items-center">
-													<div class="mr-15 bg-danger h-50 w-50 l-h-60 rounded text-center">
-														<span class="icon-Book-open font-size-24"><span class="path1"></span><span class="path2"></span></span>
-													</div>
-													<div class="d-flex flex-column font-weight-500">
-														<a href="#" class="text-dark hover-primary mb-1 font-size-16">Contemporary Art</a>
-														<span class="text-fade">Potar doe, 27 July</span>
-													</div>
-												</div>
-												<a href="#">
-													<span class="icon-Arrow-right font-size-24"><span class="path1"></span><span class="path2"></span></span>
-												</a>
-											</div>
-										</div>
-									</div>
-									<div class="box mb-15 pull-up">
-										<div class="box-body">
-											<div class="d-flex align-items-center justify-content-between">
-												<div class="d-flex align-items-center">
-													<div class="mr-15 bg-info h-50 w-50 l-h-60 rounded text-center">
-														<span class="icon-Mail font-size-24"></span>
-													</div>
-													<div class="d-flex flex-column font-weight-500">
-														<a href="#" class="text-dark hover-info mb-1 font-size-16">Live Drawing</a>
-														<span class="text-fade">Micak Doe, 12 June</span>
-													</div>
-												</div>
-												<a href="#">
-													<span class="icon-Arrow-right font-size-24"><span class="path1"></span><span class="path2"></span></span>
-												</a>
-											</div>
-										</div>
-									</div>
+									<?php endforeach ?>
 								</div>
 								<!-- ./pembelian terbaru -->
+
 								<div class="col-xl-6 col-lg-6 col-12">
 									<div class="box bg-transparent no-shadow mb-30">
 										<div class="box-header no-border pb-0">
 											<h4 class="box-title">Penjualan Terbaru</h4>
 											<ul class="box-controls pull-right d-md-flex d-none">
 												<li>
-													<button class="btn btn-primary-light px-10">Lihat Semua</button>
+													<a href="<?= site_url('transaksi/penjualan') ?>" class="btn btn-primary-light px-10">
+														Lihat Semua
+													</a>
 												</li>
 											</ul>
 										</div>
 									</div>
-									<div class="box mb-15 pull-up">
-										<div class="box-body">
-											<div class="d-flex align-items-center justify-content-between">
-												<div class="d-flex align-items-center">
-													<div class="mr-15 bg-warning h-50 w-50 l-h-60 rounded text-center">
-														<span class="icon-Book-open font-size-24"><span class="path1"></span><span class="path2"></span></span>
+									<?php foreach ($latest_sales as $ls) : ?>
+										<div class="box mb-15 pull-up">
+											<div class="box-body">
+												<div class="d-flex align-items-center justify-content-between">
+													<div class="d-flex align-items-center">
+														<div class="mr-15 bg-danger h-50 w-50 l-h-60 rounded text-center">
+															<span class="icon-Book-open font-size-24"><span class="path1"></span><span class="path2"></span></span>
+														</div>
+														<div class="d-flex flex-column font-weight-500">
+															<a href="<?= site_url('transaksi/penjualan/create/' . $ls['id_transaksi']) ?>" class="text-dark hover-primary mb-1 font-size-16"><?= $ls['id_transaksi'] ?></a>
+															<span class="text-fade"><?= $ls['nama_pelanggan'] ?>, <?= mediumdate_indo(date('Y-m-d', strtotime($ls['tanggal']))) ?></span>
+														</div>
 													</div>
-													<div class="d-flex flex-column font-weight-500">
-														<a href="#" class="text-dark hover-primary mb-1 font-size-16">Informatic Course</a>
-														<span class="text-fade">Johen Doe, 19 April</span>
-													</div>
+													<a href="<?= site_url('transaksi/penjualan/create/' . $ls['id_transaksi']) ?>">
+														<span class="icon-Arrow-right font-size-24"><span class="path1"></span><span class="path2"></span></span>
+													</a>
 												</div>
-												<a href="#">
-													<span class="icon-Arrow-right font-size-24"><span class="path1"></span><span class="path2"></span></span>
-												</a>
 											</div>
 										</div>
-									</div>
-									<div class="box mb-15 pull-up">
-										<div class="box-body">
-											<div class="d-flex align-items-center justify-content-between">
-												<div class="d-flex align-items-center">
-													<div class="mr-15 bg-primary h-50 w-50 l-h-60 rounded text-center">
-														<span class="icon-Mail font-size-24"></span>
-													</div>
-													<div class="d-flex flex-column font-weight-500">
-														<a href="#" class="text-dark hover-primary mb-1 font-size-16">Live Drawing</a>
-														<span class="text-fade">Micak Doe, 12 June</span>
-													</div>
-												</div>
-												<a href="#">
-													<span class="icon-Arrow-right font-size-24"><span class="path1"></span><span class="path2"></span></span>
-												</a>
-											</div>
-										</div>
-									</div>
-									<div class="box mb-15 pull-up">
-										<div class="box-body">
-											<div class="d-flex align-items-center justify-content-between">
-												<div class="d-flex align-items-center">
-													<div class="mr-15 bg-danger h-50 w-50 l-h-60 rounded text-center">
-														<span class="icon-Book-open font-size-24"><span class="path1"></span><span class="path2"></span></span>
-													</div>
-													<div class="d-flex flex-column font-weight-500">
-														<a href="#" class="text-dark hover-primary mb-1 font-size-16">Contemporary Art</a>
-														<span class="text-fade">Potar doe, 27 July</span>
-													</div>
-												</div>
-												<a href="#">
-													<span class="icon-Arrow-right font-size-24"><span class="path1"></span><span class="path2"></span></span>
-												</a>
-											</div>
-										</div>
-									</div>
-									<div class="box mb-15 pull-up">
-										<div class="box-body">
-											<div class="d-flex align-items-center justify-content-between">
-												<div class="d-flex align-items-center">
-													<div class="mr-15 bg-info h-50 w-50 l-h-60 rounded text-center">
-														<span class="icon-Mail font-size-24"></span>
-													</div>
-													<div class="d-flex flex-column font-weight-500">
-														<a href="#" class="text-dark hover-info mb-1 font-size-16">Live Drawing</a>
-														<span class="text-fade">Micak Doe, 12 June</span>
-													</div>
-												</div>
-												<a href="#">
-													<span class="icon-Arrow-right font-size-24"><span class="path1"></span><span class="path2"></span></span>
-												</a>
-											</div>
-										</div>
-									</div>
+									<?php endforeach ?>
+
 								</div>
 							</div>
 						</div>
